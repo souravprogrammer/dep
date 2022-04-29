e12ee70baedf11efeaf3e68badaf929d34fa6364
sourav sharma <souravsh1234567@gmail.com>
2022-04-29 20:27:17 +0530

    dd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
+import First from './First';
+import Second from './Second';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Routes>
+        <Route path='/dep' element={<First/>}  />
+
+        <Route  path="/dep/second" element={<Second/>} />
+        <Route  path="/dep/*" element={<div>no rout here</div>} />
+
+
+      </Routes>
+  
     </div>
   );
 }

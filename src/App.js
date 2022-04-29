03c6d69d52d8ebac0e80649e54b35890b8b9c832
sourav sharma <souravsh1234567@gmail.com>
2022-04-29 21:43:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
-import First from './First';
-import Second from './Second';
+import First from "./First";
+import Second from "./Second";
 
 function App() {
   return (
     <div className="App">
-
       <Routes>
-        <Route path='/dep' element={<First/>}  />
+        <Route path="/" element={<First />} />
 
-        <Route  path="/dep/second" element={<Second/>} />
-        <Route  path="/dep/*" element={<div>no rout here</div>} />
-
-
+        <Route path="/second" element={<Second />} />
+        <Route path="*" element={<div>no rout here</div>} />
       </Routes>
-  
     </div>
   );
 }
